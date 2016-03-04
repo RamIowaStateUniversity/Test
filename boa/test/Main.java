f4bbd273520143fd621ba34b74805e85289b6f73
RamIowaStateUniversity <rramu@iastate.edu>
2016-03-04 15:59:09 -0600

    dead2

--- boa/test/Main.java
+++ boa/test/Main.java
@@ -42,4 +42,16 @@ public class Main {
 				j++;				
 			}
 	}
+
+	public static void dead2(String[] a) {		
+			Scanner sc=new Scanner(System.in);
+			int y=90;
+			int b=1;
+			int j=0;
+			b=b+2;
+			while(j<2) {
+				System.out.println(compute(y));
+				j++;				
+			}
+	}
 }
